98e1b270786e49a9a773272e7cc03ef9e40622e4
kumakichi123 <asabe7729@gmail.com>
2025-09-19 23:56:30 +0900

    add GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,35 @@
 ﻿import "./globals.css";
 import Providers from "./providers";
 import SiteHeader from "./components/SiteHeader";
+import Script from "next/script";
 
 export const metadata = { title: "WakeStake" };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID || "";
+
   return (
     <html lang="en">
-      <head />
+      <head>
+        {/* gtag.js 本体 */}
+        <Script
+          id="gtag-src"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        {/* dataLayer 初期化と config */}
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            const id = '${GA_ID}';
+            if (id) {
+              gtag('js', new Date());
+              gtag('config', id, { send_page_view: true });
+            }
+          `}
+        </Script>
+      </head>
       <body>
         <Providers>
           <div style={{ minHeight: "100dvh", display: "flex", flexDirection: "column" }}>
